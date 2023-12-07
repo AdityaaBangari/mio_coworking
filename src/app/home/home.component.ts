26094a069012c771400f3fb8f28ae0e3afe9e1a8
Aditya-Bangari <adi8523696@gmail.com>
2023-12-08 03:07:17 +0530

    mobile/tablet responsive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+declare var $: any; // Declare jQuery to use it in Angular
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
 
+  }
 }
