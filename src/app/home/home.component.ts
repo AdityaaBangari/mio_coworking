05e6ce3e23159b15c65700075f5273bf36ba3de6
Aditya-Bangari <adi8523696@gmail.com>
2023-12-11 14:32:16 +0530

    import scrolltriggeer debug

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
-declare var $: any; // Declare jQuery to use it in Angular
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 // import SplitText from 'gsap/dist/SplitText';
 gsap.registerPlugin(ScrollTrigger);
 // gsap.registerPlugin(SplitText);
